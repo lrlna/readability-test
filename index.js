8c1217b367e0526426715edd5b07f17b3a8af8c9
Iryna Shestak <shestak.irina@gmail.com>
2015-10-31 22:00:24 -0700

    basic calculate function :memo:|

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ function FleschTest () {
   this.constant = 206.835;
   this.wordMultiplier = 1.015;
   this.syllableMultiplier = 84.6;
-} 
+  this.specialChars = [',', '.', ';', '?', '!', '-', ':'];
+}
+
+// calculates the score for a given string;
+FleschTest.prototype.calculate = function(string) {
+  var words = this.getWords(string);
+  var syllables = this.getSyllables(string);
+  var sentences = this.getSentences(string);
+  return this.constant - this.wordMultiplier*(words / sentences) - this.syllableMultiplier * (syllables / words);
+}
+
+// get words in an array; 
+FleschTest.prototype.getWords = function(string) {
+  var words = [];
+
+}
 
 module.exports = FleschTest;
